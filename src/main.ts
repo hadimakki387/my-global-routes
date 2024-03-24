d27126100e9f5e6026650a1a8f0ceec23e03e53d
hadimakki387 <hmakki387@gmail.com>
2024-03-24 16:46:35 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+// import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const configEnv = new ConfigService();
+  // const configEnv = new ConfigService();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(configEnv.get('POST')).then(() => {
-    console.log(`running on port ${configEnv.get('PORT')}`);
-  });
+  await app.listen(3001)
 }
 bootstrap();
