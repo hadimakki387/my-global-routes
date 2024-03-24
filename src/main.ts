81f66991bb4ad7542a0aaf57182c20299c5a6447
hadimakki387 <hmakki387@gmail.com>
2024-03-24 16:40:52 +0200

    changed port

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  const configEnv = new ConfigService();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(3000);
+  await app.listen(configEnv.get('POST')).then(() => {
+    console.log(`running on port ${configEnv.get('PORT')}`);
+  });
 }
 bootstrap();
